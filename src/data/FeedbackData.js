1e606fe93a1608bd6a7c7b74321cfbf1ba3c21a8
engribb <engribb666@gmail.com>
2023-09-15 18:15:22 +0100

    commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [  
+ { 
+    id: 1,
+    rating: 10,
+    text: 'lorem ipsum dolom sit amec consectectur adipisicing elit. consectetur vel vitae commodialais volumtum',
+},
+{
+    id: 2,
+    rating: 8,
+    text: 'lorem ipsum dolom sit amec consectectur adipisicing elit. consectetur vel vitae commodialais volumtum',
+},
+{
+    id: 3,
+    rating: 6,
+    text: 'lorem ipsum dolom sit amec consectectur adipisicing elit. consectetur vel vitae commodialais volumtum',
+},
+]
+
+export default FeedbackData
